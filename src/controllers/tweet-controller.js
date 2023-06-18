b5022b5dbb145cafea96f002bf93078689801534
unknown <sonu4004kumar@gmail.com>
2023-06-18 12:02:12 +0530

    Likes/COmments models and repos added

---
+++ src/controllers/tweet-controller.js
@@ -0,0 +1,26 @@
+import TweetService from "../services/tweet-service.js";
+
+const tweetService = new TweetService();
+
+export const createTweet = async ( req,res ) => {
+    try{
+        const data = req.body;
+        const response = await tweetService.create(data);
+
+
+        return res.status(201).json({
+            success:true,
+            message:"Successfully created a tweet",
+            data: response,
+            err:{}
+        });
+    }
+    catch(error) {
+        return res.status(500).json({
+            success:false,
+            message:"Error Encountered",
+            data: {},
+            err:error
+        });
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import express from "express"
 import {connect} from "./config/database.js";
-import Tweet from "./models/tweet.js";
+import router from "./routes/index.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(****, async ()=> {
-    console.log("server Started at ****");
+app.use("/",router)
+
+
+app.listen(6000, async ()=> {
+    console.log("server Started at 6000");
+     // mongo db connection establishment
   
     connect();
 
     console.log("Mongo DB connected");
 
+
+    /*
     Tweet.create({
         content: "This is my first tweet",
         likes: 25,
         noOfRetweets: 5,
         comment: "This is my firts comment"
     })
+    */
+
     
 })
